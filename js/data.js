e6a8d7fb64e3d228140e8f2184f6586100cf6f00
Veri5ied <43749581+Veri5ied@users.noreply.github.com>
2020-05-06 01:49:47 +0100

    First commit

---
+++ js/data.js
@@ -0,0 +1,47 @@
+let data = [
+  {
+    id: 1,
+    question: "What does HTML stand for?",
+    a: "Hyperlinks and Text Markup Language",
+    b: "Hyper Text Making Language",
+    c: "Hyper Text Markup Language",
+    d: "Hyper Text Marking Language",
+    answer: 3
+  },
+  {
+    id: 2,
+    question: "A malfunction due to an error in the program or a defect in the equipment is called what?",
+    a: "Bug",
+    b: "Byte",
+    c: "Browser",
+    d: "Crash",
+    answer: 1
+  },
+  {
+    id: 3,
+    question: "What does CSS stand for?",
+    a: "Cascading Style Sheets",
+    b: "Colorful Style Sheets",
+    c: "Creative Style Sheets",
+    d: "Creativity and Styling Sheets",
+    answer: 1
+  },
+  {
+    id: 4,
+    question: "A repository where all files are kept on computer.",
+    a: "Github",
+    b: "Directory",
+    c: "Domain",
+    d: "Recycle Bin",
+    answer: 2
+  },
+  {
+    id: 5,
+    question: "A set of data that is stored in the computer.",
+    a: "File",
+    b: "Brain",
+    c: "Network",
+    d: "Encryption",
+    answer: 1
+  },
+];
